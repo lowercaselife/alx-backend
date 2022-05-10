d97e7a5ad3797c0e53b9c766dfc651480b9e406b
danson <dansonkalaghe@gmail.com>
2022-05-10 19:14:08 +0000

    get_locale

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -17,16 +17,22 @@ class Config(object):
 
 
 # set the above class object as the configuration for the app
-app.config.from_object('1-app.Config')
+app.config.from_object('2-app.Config')
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """ GET /
     Return:
-      - 1-index.html
+      - 2-index.html
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ Determines best match for supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
